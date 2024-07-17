2b8c9ba6d1f7667d974f7018e75dc70538b98311
alexwhelan12 <alexwwh2005.12@gmail.com>
2024-07-17 17:05:57 -0600

    Started projects page, still need to update data for projects

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -2,44 +2,51 @@ import { LuGraduationCap } from "react-icons/lu";
 import React from "react";
 
 const projects = [{
-    title: "Project 1",
-    location: "Calgary, AB",
-    description: "This is a description of project 1",
-    icon: React.createElement(LuGraduationCap),
-    date: "2024"
-},{
-    title: "Project 1",
-    location: "Calgary, AB",
-    description: "This is a description of project 1",
-    icon: React.createElement(LuGraduationCap),
-    date: "2024"
+    title: "Solar Car Helio Telemetry Dashboard",
+    description: "With over 100,000 mothly active bot users, Gippity AI is the most popular AI platform for developers.",
+    image: "/linear.webp",
+    techStack: [{id:1, name: "Tailwind CSS", designation: "Styling", image:"/tailwindcss-icon.png"},
+        {id:2, name: "Next.js", designation: "React Framework", image:"/tailwindcss-icon.png"},
+        {id:3, name: "TypeScript", designation: "Logic", image:"/tailwindcss-icon.png"}],
 },
 {
-    title: "Project 1",
-    location: "Calgary, AB",
-    description: "This is a description of project 1",
-    icon: React.createElement(LuGraduationCap),
-    date: "2024"},
-{
-    title: "Project 1",
-    location: "Calgary, AB",
-    description: "This is a description of project 1",
-    icon: React.createElement(LuGraduationCap),
+    title: "Handheld Video Game Console",
+    description: "With over 100,000 mothly active bot users, Gippity AI is the most popular AI platform for developers.",
+    image: "/linear.webp",
+    techStack: [{id:1, name: "Tailwind CSS", designation: "Styling", image:"/tailwindcss-icon.png"},
+        {id:2, name: "Next.js", designation: "React Framework", image:"/tailwindcss-icon.png"},
+        {id:3, name: "TypeScript", designation: "Logic", image:"/tailwindcss-icon.png"}],
 },
 {
-    title: "Project 1",
-    location: "Calgary, AB",
-    description: "This is a description of project 1",
-    icon: React.createElement(LuGraduationCap),
-    date: "2024"
-},{
-    title: "Project 1",
-    location: "Calgary, AB",
-    description: "This is a description of project 1",
-    icon: React.createElement(LuGraduationCap),
-    date: "2024"
-}
-];
+    title: "Automated, Self Sustaining Garden",
+    description: "With over 100,000 mothly active bot users, Gippity AI is the most popular AI platform for developers.",
+    image: "/linear.webp",
+    techStack: [{id:1, name: "Tailwind CSS", designation: "Styling", image:"/tailwindcss-icon.png"},
+        {id:2, name: "Next.js", designation: "React Framework", image:"/tailwindcss-icon.png"},
+        {id:3, name: "TypeScript", designation: "Logic", image:"/tailwindcss-icon.png"}],
+},
+{
+    title: "Disaster Relief Program",
+    description: "With over 100,000 mothly active bot users, Gippity AI is the most popular AI platform for developers.",
+    image: "/linear.webp",
+    techStack: [{id:1, name: "Tailwind CSS", designation: "Styling", image:"/tailwindcss-icon.png"},
+        {id:2, name: "Next.js", designation: "React Framework", image:"/tailwindcss-icon.png"},
+        {id:3, name: "TypeScript", designation: "Logic", image:"/tailwindcss-icon.png"}],
+},
+{
+    title: "Flight Information Interface",
+    description: "With over 100,000 mothly active bot users, Gippity AI is the most popular AI platform for developers.",
+    image: "/linear.webp",
+    techStack: [{id:1, name: "Tailwind CSS", designation: "Styling", image:"/tailwindcss-icon.png"},
+        {id:2, name: "Next.js", designation: "React Framework", image:"/tailwindcss-icon.png"},
+        {id:3, name: "TypeScript", designation: "Logic", image:"/tailwindcss-icon.png"}],
+},
+{
+    title: "More Projects Coming Soon...",
+    description: "With over 100,000 mothly active bot users, Gippity AI is the most popular AI platform for developers.",
+    image: "/linear.webp",
+    techStack: [],
+}];
 
 
 export default projects;
--- components/ProjectsPage.tsx
+++ components/ProjectsPage.tsx
@@ -5,6 +5,10 @@ import Header from './Header';
 import { motion, AnimatePresence } from 'framer-motion';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import projects from '@/app/data/projects';
+import { WobbleCard } from './ui/wobble-card';
+import Image from 'next/image';
+import { AnimatedTooltip } from './ui/animated-tooltip';
+
 
 
 function ProjectsPage() {
@@ -13,16 +17,30 @@ function ProjectsPage() {
   return (
     <section>
       <Header />
-      <VerticalTimeline>
-        {projects.map((experience, index) => (
-          <React.Fragment key={index}>
-            <VerticalTimelineElement visible={true}>
-              <h3 className="text-red-500">{experience.title}</h3>
-              <p>{experience.description}</p>
-            </VerticalTimelineElement>
-          </React.Fragment>
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-[90rem] w-full h-full absolute top-[10rem] left-[15rem]'>
+        {projects.map((project, index) => (
+          <WobbleCard key={index} className="m-5 p-5">
+              <div className="max-w-xs">
+                <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+                  {project.title}
+                </h2>
+                <p className="mt-4 text-left  text-base/6 text-neutral-200">
+                  {project.description}
+                </p>
+                <div className='flex flex-row gap-5 mt-3'>
+                  <AnimatedTooltip items={project.techStack || [] } />
+                </div>
+              </div>
+              {/* <Image
+                src="/linear.webp"
+                width={500}
+                height={500}
+                alt="linear demo image"
+                className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl"
+              /> */}
+          </WobbleCard>
         ))}
-      </VerticalTimeline>
+      </div>
     </section>
   )
 }
