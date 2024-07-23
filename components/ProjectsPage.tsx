0a193dd63a3cff0d8df9cc60b08c6092575234d5
alexwhelan12 <alexwh2005.12@gmail.com>
2024-07-23 01:50:28 -0600

    trying to make it responsive but preview button is buggin out

--- components/ProjectsPage.tsx
+++ components/ProjectsPage.tsx
@@ -18,42 +18,61 @@ function ProjectsPage() {
   return (
     <section>
       <Header />
-      <motion.div className='grid grid-cols-1 xl:grid-cols-3 gap-4 max-w-[90rem] w-full h-[70rem] absolute top-[10rem] left-[15rem] xl:w-[80rem] lg:left-[7.5rem]'
-        initial={{opacity: 0}}
-        animate={{
-            opacity: 1,
-            transition: { delay:0, duration: 0.8, ease: 'easeInOut'}
-        }}
-      >
-        {projects.map((project, index) => (
-          <WobbleCard key={index} className="m-5 p-5 col-span-3">
-              <div className="max-w-xs">
-                <h2 className="text-left text-balance text-base md:text-xl lg:text-4xl font-semibold tracking-[-0.015em] text-white font-mono">
-                  {project.title}
-                </h2>
-                <p className="mt-4 text-left  text-base/6 text-neutral-200 font-mono">
-                  {project.description}
-                </p>
-                <div className='flex flex-row gap-5 mt-3'>
-                  <AnimatedTooltip items={project.techStack || [] } />
-                </div>
-              </div>
-              {project.link && <motion.a className='text-white z-10 absolute top-[23rem] bg-[#062056] p-2 rounded-lg font-mono mt-2' 
-                  href={project.link} target="_blank" rel="noreferrer"
-                  whileHover={{ scale: 1.1, 
-                    transition: { duration: 0.2 }
-                  }}
-                  >Preview</motion.a>}
-              <Image
-                src={project.image}
-                width={500}
-                height={500}
-                alt="linear demo image"
-                className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl -z-10"
-              />
-          </WobbleCard>
-        ))}
-      </motion.div>
+      <motion.div
+    className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 max-w-[90rem] w-full absolute top-[10rem] left-[15rem] xl:w-[80rem] lg:left-[7.5rem] md:left-5 md:w-[45rem] md:m-1"
+    initial={{ opacity: 0 }}
+    animate={{
+      opacity: 1,
+      transition: { delay: 0, duration: 0.8, ease: 'easeInOut' },
+  }}
+>
+  {projects.map((project, index) => (
+    <WobbleCard
+      key={index}
+      className="m-5 p-5 col-span-1 relative aspect-square flex flex-col justify-between w-full md:h-[25rem] lg:w-[28rem] xl:h-[28rem]"
+    >
+      <div className="h-full flex flex-col justify-between md:w-[15rem]">
+        <div>
+          <h2 className="text-left text-balance text-base md:text-xl lg:text-4xl font-semibold tracking-[-0.015em] text-white font-mono">
+            {project.title}
+          </h2>
+          <p className="mt-4 text-left text-base/6 text-neutral-200 font-mono">
+            {project.description}
+          </p>
+        </div>
+        <div className={`flex flex-row gap-5 mt-3 md:absolute md:top-[13rem] 
+          ${index === 0 ?"xl:top-[19.5rem]" : "xl:top-[18rem]" }`}
+          
+          >
+          <AnimatedTooltip items={project.techStack || []}/>
+        </div>
+      </div>
+      {project.link && (
+        <motion.a
+          className="text-white z-10 absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-[#062056] p-2 rounded-lg font-mono md:left-20 md:bottom-[4.5rem] xl:bottom-[1rem]"
+          href={project.link}
+          target="_blank"
+          rel="noreferrer"
+          whileHover={{
+            scale: 1.1,
+            transition: { duration: 0.2 },
+          }}
+        >
+          Preview
+        </motion.a>
+      )}
+      <Image
+        src={project.image}
+        width={500}
+        height={500}
+        alt="linear demo image"
+        className="absolute -right-4 lg:-right-[40%] md:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl -z-10 md:opacity-85"
+      />
+    </WobbleCard>
+  ))}
+</motion.div>
+
+
     </section>
   )
 }
