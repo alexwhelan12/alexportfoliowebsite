9701ca29d5a5a92ffd6645b6bc9a579817f55f15
alexwhelan12 <alexwh2005.12@gmail.com>
2024-07-29 23:19:35 -0600

    projects page should be responsive now!

--- components/ProjectsPage.tsx
+++ components/ProjectsPage.tsx
@@ -19,7 +19,7 @@ function ProjectsPage() {
     <section>
       <Header />
       <motion.div
-      className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 max-w-[90rem] w-full absolute top-[10rem] left-[15rem] xl:w-[80rem] lg:left-[7.5rem] md:left-5 md:w-[45rem] md:m-1 sm:w-[30rem]"
+      className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 max-w-[90rem] w-full absolute top-[10rem] left-[15rem] xl:w-[80rem] lg:left-[7.5rem] md:left-5 md:w-[45rem] md:m-1 sm:w-[30rem] sm:left-[5rem]"
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
