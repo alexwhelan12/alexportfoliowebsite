8335441a4af93aa080d4acb770ad81a5a172e9ce
alexwhelan12 <alexwwh2005.12@gmail.com>
2024-08-02 21:57:54 -0600

    more responsive stuff on project page

--- components/ProjectsPage.tsx
+++ components/ProjectsPage.tsx
@@ -19,7 +19,7 @@ function ProjectsPage() {
     <section>
       <Header />
       <motion.div
-      className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 max-w-[90rem] w-full absolute top-[10rem] left-[15rem] xl:w-[80rem] xl:left-[7rem] lg:w-[60rem] lg:left-[5.75rem] md:w-[35rem] md:m-1 md:left-[2.25rem] sm:w-[30rem] sm:left-[5rem]"
+      className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 max-w-[90rem] w-full absolute top-[10rem] left-[15rem] xl:w-[80rem] xl:left-[7rem] lg:w-[60rem] lg:left-[5.75rem] md:w-[35rem] md:m-1 md:left-[2.25rem] sm:w-[30rem] sm:left-[5rem] 2xl:left-[14.5rem] 2xl:w-[100rem]"
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
@@ -28,19 +28,19 @@ function ProjectsPage() {
   {projects.map((project, index) => (
     <WobbleCard
       key={index}
-      className="m-5 p-5 col-span-1 relative aspect-square flex flex-col justify-between w-full md:h-[25rem] lg:w-[28rem] xl:h-[28rem]"
+      className="m-5 p-5 col-span-1 relative aspect-square flex flex-col justify-between w-full md:h-[25rem] lg:w-[28rem] xl:h-[28rem"
     >
       <div className="h-full flex flex-col justify-between md:w-[15rem]">
         <div>
           <h2 className="text-left text-balance text-base md:text-xl lg:text-4xl font-semibold tracking-[-0.015em] text-white font-mono">
             {project.title}
           </h2>
-          <p className="mt-4 text-left text-base/6 text-neutral-200 font-mono">
+          <p className={`${index === 0 ? "xl:mt-1" : ""} mt-4 text-left text-base/6 text-black font-mono font-bold bg-white rounded-lg bg-opacity-50 p-2`}>
             {project.description}
           </p>
         </div>
         <div className={`flex flex-row gap-5 mt-3 absolute md:top-[13rem] 
-          ${index === 0 ?"xl:top-[19.5rem]" : "xl:top-[18rem]" } sm:top-[8rem] sm:left-[2rem]`}
+          ${index === 0 ?"xl:top-[18.25rem] xl:left-[2rem]" : "xl:top-[18rem]" } sm:top-[8rem] sm:left-[2rem]`}
           
           >
           <AnimatedTooltip items={project.techStack || []}/>
@@ -48,7 +48,7 @@ function ProjectsPage() {
       </div>
       {project.link && (
         <motion.a
-          className="text-white z-10 absolute bottom-5 bg-[#062056] p-2 rounded-lg font-mono md:bottom-[4.5rem] xl:bottom-[1rem] xl:left-8 sm:left-8 sm:bottom-[17rem]"
+          className={`${index === 0 ? "xl:-bottom-3 xl:left-9" : ""} text-white z-10 absolute bottom-5 bg-[#062056] p-2 rounded-lg font-mono md:bottom-[4.5rem] xl:bottom-[1rem] xl:left-8 sm:left-8 sm:bottom-[17rem]`}
           href={project.link}
           target="_blank"
           rel="noreferrer"
@@ -65,7 +65,7 @@ function ProjectsPage() {
         width={500}
         height={500}
         alt="linear demo image"
-        className={`absolute -right-4 lg:-right-[40%] md:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl -z-10 md:opacity-85 sm:-right-[20%] sm:-bottom-[1.5rem] ${index === 1 ? "sm:-bottom-[5.5rem] sm:-right-[8rem]" : ""}`}
+        className={`absolute -right-4 lg:-right-[40%] md:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl -z-10 md:opacity-65 sm:-right-[20%] sm:-bottom-[1.5rem] ${index === 1 ? "sm:-bottom-[5.5rem] sm:-right-[8rem]" : ""}`}
       />
     </WobbleCard>
   ))}
