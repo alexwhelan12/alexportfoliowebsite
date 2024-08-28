0a8db93cae97832cc3f0e63b6aa638f1c7c86ae9
alexwhelan12 <alexwwh2005.12@gmail.com>
2024-08-28 17:41:15 -0600

    quick fix

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -48,7 +48,7 @@ function ContactPage() {
         }}
       >
         <div className="contactsBody[background-image:radial-gradient(88%_100%_at_top,rgba(255,255,255,0.5),rgba(255,255,255,0))] p-5 flex items-center justify-center xl:w-[50rem] sm:w-[30rem] sm:left-[4.5rem] absolute rounded-xl mt-10 border-orange-600 border">
-          <section className='contact-page flex items-center justify-center mt-16'>
+          <section className='contact-page flex items-center justify-center'>
             <div className="contact-form flex flex-col items-center justify-center text-white">
               <h2 className='font-mono text-4xl p-5 text-white'>Contact Me</h2>
               <p className='text-xl font-mono text-slate-100 ml-5'>Feel free to message me about any inquiries!</p>
@@ -62,7 +62,7 @@ function ContactPage() {
                 <div className="form-group p-5 w-52 h-20 mb-10">
                   <textarea id="message" name="message" required style={{"resize":"none"}} className='rounded-md w-[500px] sm:w-[440px] sm:ml-[7.25rem] h-[100px] outline-none text-black p-2 ml-24' placeholder='Message'/>
                 </div>
-                  <button type="submit" className="submit-button bg-orange-600 p-2 rounded-md mt-8 text-white sm:w-32 hover:bg-orange-500 ml-72">Send Message</button>
+                  <button type="submit" className="submit-button bg-orange-600 p-2 rounded-md mt-8 text-white sm:w-32 hover:bg-orange-500 ml-72 w-fit">Send</button>
               </form>
               {successMessage && <p className="success-message text-white mt-1 mr-1.5">Message sent successfully!</p>}
             </div>
